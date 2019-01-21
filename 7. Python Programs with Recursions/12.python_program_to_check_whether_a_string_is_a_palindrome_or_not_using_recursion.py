ef574fcbdb0b8280a0099380a2864e1541409c00
rajesh <rajeshsurya1994@gmail.com>
2019-01-21 16:47:06 +0530

    Further python excercise on Recursion in Python on 21-01-2019

--- 7. Python Programs with Recursions/12.python_program_to_check_whether_a_string_is_a_palindrome_or_not_using_recursion.py
+++ 7. Python Programs with Recursions/12.python_program_to_check_whether_a_string_is_a_palindrome_or_not_using_recursion.py
@@ -14,7 +14,7 @@ def palindrome(string):
     if len(string) < 1:
         return True
     else:
-        if string[-0] == string[-1]:
+        if string[0] == string[-1]:
             return palindrome(string[1 : -1])
         else:
             return False
