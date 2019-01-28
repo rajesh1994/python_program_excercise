5e592b6d27b6925384679130ac7b448bd862c5ed
rajesh <rajeshsurya1994@gmail.com>
2019-01-28 11:33:26 +0530

    Further python excercise on File Handling in Python on 28-01-2019

---
+++ 11. Python Programs/1_python_program to_convert_a_string_to_lowercase.py~
@@ -0,0 +1,3 @@
+input = str(input("Enter a string to change it into lowercase: "))
+b = input.lower()
+print (b)
---
+++ 11. Python Programs/2_python_program_replace_space_with_underscore.py
@@ -0,0 +1,4 @@
+a = str(input("Enter text to convert it into another form: "))
+b = a.replace(" ", "_").lower()
+c = '_' + b + '.py'
+print (c)
